8866a53c0c5301eeb4f6b571fda4898b923b0e26
luis <lcornejo20@jaumebalmes.net>
2023-01-23 20:18:52 +0100

    ejercicios a luis

---
+++ exercici2_A.py
@@ -0,0 +1,6 @@
+diccionario = {'coches':10, 'motos':5, 'bici':15, 'jet':8, 'helicoptero':7, 'avion':4}
+"""esto es un diccionario"""
+print(len(diccionario))
+print(diccionario.values())
+print(diccionario.popitem())
+"""imprimimos por pantalla"""
