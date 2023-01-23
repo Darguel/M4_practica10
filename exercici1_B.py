369d7694729dac1b21ad73f65d0e5589be0d051e
David Arguelles <darguel20@jaumebalmes.net>
2023-01-23 20:15:55 +0100

    2 primeros ejercicios David

---
+++ exercici1_B.py
@@ -0,0 +1,4 @@
+def añonacimiento():
+    """año actual - años de la persona para saber año que nacio"""
+    print(2023-19)
+(añonacimiento())
---
+++ exercici2_B.py
@@ -0,0 +1,7 @@
+mitupla = (1,2,3,4,5,6)
+"""para mostrar la longitutd de la tupla"""
+print(len(mitupla))
+"""para buscar el "3" en la tupla"""
+print(3 in mitupla)
+"""para contar cuantas veces aparece "2" en la tupla"""
+print(mitupla.count(2))
